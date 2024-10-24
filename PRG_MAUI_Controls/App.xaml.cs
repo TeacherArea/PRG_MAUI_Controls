e8c9bfb6a2342d9ee07a4fda315c545a9055cac2
TeacherArea <sewilius.berg@initialize.se>
2024-10-24 14:11:05 +0200

    Some bugsfixes

--- PRG_MAUI_Controls/App.xaml.cs
+++ PRG_MAUI_Controls/App.xaml.cs
@@ -8,5 +8,17 @@
 
             MainPage = new AppShell();
         }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            #if WINDOWS
+                window.Width = 480;
+                window.Height = 800;
+            #endif
+
+            return window;
+        }
+
     }
 }
